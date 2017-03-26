3d5a3261de1ba663d3a9c832a8731c792352e83b
Igor C. A. de Lima <igorcadelima@gmail.com>
2017-03-26 14:11:28 +0000

    Solve second query of exercise 2

--- puppy_shelters/puppy_query.py
+++ puppy_shelters/puppy_query.py
@@ -24,3 +24,44 @@ def query_one():
     for item in result:
         print item[0]
 
+def query_two():
+    """Query all of the puppies that are less than 6 months old organized by the youngest first"""
+    today = datetime.date.today()
+    if passesLeapDay(today):
+        sixMonthsAgo = today - datetime.timedelta(days = 183)
+    else:
+        sixMonthsAgo = today - datetime.timedelta(days = 182)
+    result = session.query(Puppy.name, Puppy.dateOfBirth)\
+        .filter(Puppy.dateOfBirth >= sixMonthsAgo)\
+        .order_by(Puppy.dateOfBirth.desc())
+
+    # print the result with puppy name and dob
+    for item in result:
+        print "{name}: {dob}".format(name=item[0], dob=item[1])
+
+# Helper Methods
+def passesLeapDay(today):
+    """
+    Returns true if most recent February 29th occured after or exactly 183 days ago (366 / 2)
+    """
+    thisYear = today.timetuple()[0]
+    if isLeapYear(thisYear):
+        sixMonthsAgo = today - datetime.timedelta(days = 183)
+        leapDay = datetime.date(thisYear, 2, 29)
+        return leapDay >= sixMonthsAgo
+    else:
+        return False
+        
+def isLeapYear(thisYear):
+    """
+    Returns true iff the current year is a leap year.
+    Implemented according to logic at https://en.wikipedia.org/wiki/Leap_year#Algorithm
+    """
+    if thisYear % 4 != 0:
+        return False
+    elif thisYear % 100 != 0:
+        return True
+    elif thisYear % 400 != 0:
+        return False
+    else:
+        return True
