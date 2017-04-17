5bc461911ed652cfb7a9c5f843e07cf58931c959
Igor C. A. de Lima <igorcadelima@gmail.com>
2017-04-17 01:49:21 +0000

    Add hola page

--- 03_Making_a_Web_Server/webserver.py
+++ 03_Making_a_Web_Server/webserver.py
@@ -3,16 +3,22 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 class WebserverHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
+            output = ""
             if self.path.endswith("/hello"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
 
-                output = ""
                 output += "<html><body>Hello!</body></html>"
                 self.wfile.write(output)
+            elif self.path.endswith("/hola"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                output += "<html><body>&#161Hola! <a href='/hello'>Back to Hello</a></body></html>"
+                self.wfile.write(output)
                 print output
-                return
         except IOError:
             self.send_error(404, "File Not Found %s" % self.path)
 
